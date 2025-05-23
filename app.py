9b16c7721d035a29218792c110ece33dfc6e8ff1
gokul0420 <124028423+gokul0420@users.noreply.github.com>
2025-05-23 21:08:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-df = pd.read_excel('dishes.xlsx')
-
+df = pd.read_excel('dishes.xlsx',engine='openpyxl')
 
 all_ingredients = set()
 dish_ingredients = []
